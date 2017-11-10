d5d3ec681e63291953f40148c66451835f32e56b
soeyusuke <s1240231@gmail.com>
2017-11-10 21:40:17 +0900

    [ADD] first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gocraft/dbr"
+	"github.com/labstack/echo"
+)
+
+var (
+	tablename = "bookstate"
+	conn, _   = dbr.Open("mysql", "username:api@/book_api", nil)
+	sess      = conn.NewSession(nil)
+)
+
+func main() {
+	e := echo.New()
+
+	// Routing
+	e.GET("/book/api/fetch/:title", PullBookInfo)
+	e.GET("/book/api/fetch/all", PullBooksInfo)
+	e.PUT("/book/api/update", UpdateBookInfo)
+	e.POST("/book/api/post", PostBookInfo)
+	e.DELETE("/book/api/delete/:id", DeleteBookInfo)
+
+	e.Logger.Fatal(e.Start(":9090"))
+}
